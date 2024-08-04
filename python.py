0bfba25055e23ac54f860959bb7d33f990e5fbbb
praneeth demo <praneethgovindaraja@gmail.com>
2024-08-04 14:57:15 +0530

    completed

---
+++ python.py
@@ -0,0 +1,6 @@
+name = input (" enter your name ")
+print ("hi ",name )
+find = int (23)
+steps = int (input("enter your steps "))
+total = steps / find 
+print (" here is your Calorie count",total )
