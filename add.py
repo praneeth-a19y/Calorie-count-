92e206ab1809405c4e2b9b92991b39166a8510c9
praneeth demo <praneethgovindaraja@gmail.com>
2024-08-05 18:05:00 +0530

    add.py

---
+++ add.py
@@ -0,0 +1,4 @@
+f = int (input (  " enter any number "))
+h = int (input (  " enter any number "))
+k = f+h 
+print (k," thanks for using ")
--- python.py
+++
@@ -1,7 +0,0 @@
-name = input (" enter your name ")
-print ("hi ",name )
-find = int (23)
-steps = int (input("enter your steps "))
-total = steps / find 
-print (" here is your Calorie count",total )
-print (" thanks for using this ")
